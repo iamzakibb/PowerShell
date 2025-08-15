a20224ac9ae3d5a81a03980efca55d24138933cc
Yasir <yasir@Yasirs-MacBook-Air.local>
2025-08-15 22:25:49 +0530

    changes made

--- samplescriptforlogicapps.ps1
+++ samplescriptforlogicapps.ps1
@@ -26,7 +26,7 @@ if (-not $accessToken) {
     exit 1
 }
 
-$uri = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Logic/workflows/$logicAppName?api-version=2019-05-01"
+$uri = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Logic/workflows/$logicAppName/?api-version=2019-05-01"
 
 $response = Invoke-RestMethod -Method Get -Uri $uri -Headers @{
     Authorization = "Bearer $accessToken"
