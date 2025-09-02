9c2d94971e75721ce5e02a96f8d77e3eeef98655
Yasir Mushtaq <ymushtaq451@gmail.com>
2025-09-02 19:52:07 +0530

    changes made

--- HubspotTableExport.ps1
+++ HubspotTableExport.ps1
@@ -1,5 +1,5 @@
 # Replace with your HubSpot API token and table ID/name
-$apiToken = "pat-na1-167a2bc3-2417-41e1-a8a0-c55f7f12b2bf"
+$apiToken = ""
 $tableIdOrName = "6814391"
 $endpoint = "https://api.hubapi.com/cms/v3/hubdb/tables/$tableIdOrName/export"
 
